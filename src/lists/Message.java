418d088a6b85bc7b0a3a25176647e0b7d62676d7
alexmicklow <alexmicklow@10.139.113.61>
2015-09-14 23:00:06 -0400

    Message should be peachy

--- src/lists/Message.java
+++ src/lists/Message.java
@@ -1,10 +1,17 @@
 package lists;
 
+import java.util.EmptyStackException;
+
 public class Message {
 	
+	int packet;
+	String message;
+	Node head;
 	
 	public Message(int packet, String message) {
-		// TODO Auto-generated constructor stub
+		this.packet = packet;
+		this.message = message;
+		this.head = null;
 	}
 
 	/**
@@ -14,15 +21,25 @@ public class Message {
 	 */
 	public void push(int packet, String message){
 		
+		head = new Node(head, message, packet);
+		
 	}
 	
 	/**
-	 * inserts packet in decending order
+	 * inserts packet in descending order
 	 * @param packet
 	 * @param message
 	 */
 	public void insert(int packet, String message){
-		
+		Node current = head;
+		if(packet >= head.data.packet || head.equals(null)){
+			head = new Node(head, message, packet);
+		} else {
+			while(packet < current.next.data.packet){
+				current = current.next;
+			}
+			current.next = new Node(current.next, message, packet);
+		}
 	}
 	
 	/**
@@ -30,12 +47,23 @@ public class Message {
 	 * @return
 	 */
 	public Message pop(){
-		
+		if(this.head == null){
+			throw new EmptyStackException();
+		}
+		Message temp = head.data;
+		head = head.next;
+		return temp;
 	}
 	
 	private class Node{
-		Node node;
-		String message;
-		int packet;
+		Node next;
+		Message data;
+		
+		public Node(Node next, String message, int packet){
+			
+			this.next = next;
+			this.data = new Message(packet, message);
+			
+		}
 	}
 }
