1eafa0f65f9d6df9d983afcbf2db814b8594d1a8
mrpatlov <mrpatlov@ncsu.edu>
2015-09-18 09:41:50 -0400

    Output done (have not tested)

--- src/io/OutputFormatter.java
+++ src/io/OutputFormatter.java
@@ -1,6 +1,9 @@
 package io;
 
+import java.io.*;
+
 import lists.ListOfMessages;
+import lists.Message;
 
 /**
  * This class formats output properly by 
@@ -9,20 +12,67 @@ import lists.ListOfMessages;
  * 
  * The member doing this class needs to do unit tests for 
  * this class and Message
- * @author Jeremy
+ * @author Michael Patlovich
  *
  */
 
 public class OutputFormatter {
+	
+	//This stores the file for the printer, closes at end of writeData
+	private PrintWriter inFile;
+	//This stores the message number
+	private int messageNum;
+	//This stores the message text
+	private String messageText;
+	//This stores the packet for the current message
+	private int packet;
+	//This stores the packet for the previous message
+	private int prevPacket;
+	//This stores the current message that you are on
+	private Message message;
 
+	/**
+	 * This takes a string and creates a writer to an output file and catches if the file does not exist.
+	 * @param string, the file name for the output file
+	 */
 	public OutputFormatter(String string) {
-		// TODO Auto-generated constructor stub
+		try {
+			inFile = new PrintWriter(string);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	 * This method takes a list of messages and prints them out to an output file 
+	 * printing errors where needed.
+	 * @param messages, a list of all the messages
+	 */
 	public void writeData(ListOfMessages messages) {
-		// TODO Auto-generated method stub
-		
+		messageNum = messages.peek();
+		message = messages.pop();
+		while(message != null) {
+			inFile.println("--- Message " + messageNum);
+			packet = message.peek();
+			prevPacket = packet;
+			messageText = message.pop();
+			while(messageText != null) {
+					if(packet != prevPacket + 1 || packet != prevPacket) {
+						inFile.println("WARNING: packet " + packet + " of message " + messageNum + " is missing");
+					} else {
+						inFile.println(messageText);
+					}
+					prevPacket = packet;
+					packet = message.peek();
+					messageText = message.pop();
+			}
+			inFile.println("--- End Message " + messageNum);
+			inFile.println();
+			messageNum = messages.peek();
+			message = messages.pop();
+		}
+		inFile.close();
 	}
-	//See Receiver class for how this class is called. Either try to follow those method
-	//headers here or rewrite those method calls in Receiver.
+	
 }
